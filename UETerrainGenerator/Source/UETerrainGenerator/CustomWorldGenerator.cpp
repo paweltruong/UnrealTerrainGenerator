33e118ccb44018d6735f9d8bc758645f9c90b4e8
DESKTOP-PEPNLPD\pawel <paweltruong@gmail.com>
2022-03-10 16:25:24 +0100

    custom terrain generator in level

---
+++ UETerrainGenerator/Source/UETerrainGenerator/CustomWorldGenerator.cpp
@@ -0,0 +1,108 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CustomWorldGenerator.h"
+//#include "VoxelGeneratorExample.h"
+#include "FastNoise/VoxelFastNoise.inl"
+#include "VoxelMaterialBuilder.h"
+
+TVoxelSharedRef<FVoxelGeneratorInstance> UCustomWorldGenerator::GetInstance()
+{
+	return MakeVoxelShared<FCustomWorldGeneratorInstance>(*this);
+}
+
+///////////////////////////////////////////////////////////////////////////////
+
+FCustomWorldGeneratorInstance::FCustomWorldGeneratorInstance(const UCustomWorldGenerator& MyGenerator)
+	: Super(&MyGenerator)
+	, NoiseHeight(MyGenerator.NoiseHeight)
+	, Seed(MyGenerator.Seed)
+{
+}
+
+void FCustomWorldGeneratorInstance::Init(const FVoxelGeneratorInit& InitStruct)
+{
+	Noise.SetSeed(Seed);
+}
+
+v_flt FCustomWorldGeneratorInstance::GetValueImpl(v_flt X, v_flt Y, v_flt Z, int32 LOD, const FVoxelItemStack& Items) const
+{
+	const float Height = Noise.GetPerlin_2D(X, Y, 0.01f) * NoiseHeight;
+
+	// Positive value -> empty voxel
+	// Negative value -> full voxel
+	// Value positive when Z > Height, and negative Z < Height
+	float Value = Z - Height;
+
+	// The voxel value is clamped between -1 and 1. That can result in a bad gradient/normal. To solve that we divide it
+	Value /= 5;
+
+	return Value;
+}
+//Sphere world Implementation
+//v_flt FCustomWorldGeneratorInstance::GetValueImpl(v_flt X, v_flt Y, v_flt Z, int32 LOD, const FVoxelItemStack& Items) const
+//{
+//	// Radius of the world
+//	const float Radius = 100.f;
+//
+//	// Normalize the position to get the 3D noise sample position
+//	const FVector SamplePosition = FVector(X, Y, Z).GetSafeNormal();
+//
+//	// Compute noise. Note that the noise has a much higher frequency since the sample position is normalized
+//	const float Height = Radius + Noise.GetPerlin_3D(SamplePosition.X, SamplePosition.Y, SamplePosition.Z, 1.f) * NoiseHeight;
+//
+//	// Value = DistanceFromCenter - Height
+//	float Value = FVector(X, Y, Z).Size() - Height;
+//
+//	// Smoother gradient
+//	Value /= 5;
+//
+//	return Value;
+//}
+
+FVoxelMaterial FCustomWorldGeneratorInstance::GetMaterialImpl(v_flt X, v_flt Y, v_flt Z, int32 LOD, const FVoxelItemStack& Items) const
+{
+	FVoxelMaterialBuilder Builder;
+
+	// RGB
+	Builder.SetMaterialConfig(EVoxelMaterialConfig::RGB);
+	Builder.SetColor(FColor::Red);
+
+	// Single index
+	//Builder.SetMaterialConfig(EVoxelMaterialConfig::SingleIndex);
+	//Builder.SetSingleIndex(0); 
+
+	// Multi index
+	//Builder.SetMaterialConfig(EVoxelMaterialConfig::MultiIndex);
+	//Builder.AddMultiIndex(0, 0.5f);
+	//Builder.AddMultiIndex(1, 0.5f);
+
+	return Builder.Build();
+}
+
+TVoxelRange<v_flt> FCustomWorldGeneratorInstance::GetValueRangeImpl(const FVoxelIntBox& Bounds, int32 LOD, const FVoxelItemStack& Items) const
+{
+	// Return the values that GetValueImpl can return in Bounds
+	// Used to skip chunks where the value does not change
+	// Be careful, if wrong your world will have holes!
+	// By default return infinite range to be safe
+	return TVoxelRange<v_flt>::Infinite();
+
+	// Example for the GetValueImpl above
+
+	// Noise is between -1 and 1
+	const TVoxelRange<v_flt> Height = TVoxelRange<v_flt>(-1, 1) * NoiseHeight;
+
+	// Z can go from min to max
+	TVoxelRange<v_flt> Value = TVoxelRange<v_flt>(Bounds.Min.Z, Bounds.Max.Z) - Height;
+
+	Value /= 5;
+
+	return Value;
+}
+
+FVector FCustomWorldGeneratorInstance::GetUpVector(v_flt X, v_flt Y, v_flt Z) const
+{
+	// Used by spawners
+	return FVector::UpVector;
+}
---
+++ UETerrainGenerator/Source/UETerrainGenerator/CustomWorldGenerator.h
@@ -0,0 +1,51 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+
+#include "CoreMinimal.h"
+#include "FastNoise/VoxelFastNoise.h"
+#include "VoxelGenerators/VoxelGeneratorHelpers.h"
+#include "CustomWorldGenerator.generated.h"
+
+
+UCLASS(Blueprintable)
+class UETERRAINGENERATOR_API UCustomWorldGenerator : public UVoxelGenerator
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Generator")
+		float NoiseHeight = 10.f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Generator")
+		int32 Seed = 1337;
+
+	//~ Begin UVoxelGenerator Interface
+	virtual TVoxelSharedRef<FVoxelGeneratorInstance> GetInstance() override;
+	//~ End UVoxelGenerator Interface
+};
+
+class FCustomWorldGeneratorInstance : public TVoxelGeneratorInstanceHelper<FCustomWorldGeneratorInstance, UCustomWorldGenerator>
+{
+public:
+	using Super = TVoxelGeneratorInstanceHelper<FCustomWorldGeneratorInstance, UCustomWorldGenerator>;
+
+	explicit FCustomWorldGeneratorInstance(const UCustomWorldGenerator& MyGenerator);
+
+	//~ Begin FVoxelGeneratorInstance Interface
+	virtual void Init(const FVoxelGeneratorInit& InitStruct) override;
+
+	v_flt GetValueImpl(v_flt X, v_flt Y, v_flt Z, int32 LOD, const FVoxelItemStack& Items) const;
+	FVoxelMaterial GetMaterialImpl(v_flt X, v_flt Y, v_flt Z, int32 LOD, const FVoxelItemStack& Items) const;
+
+	TVoxelRange<v_flt> GetValueRangeImpl(const FVoxelIntBox& Bounds, int32 LOD, const FVoxelItemStack& Items) const;
+
+	virtual FVector GetUpVector(v_flt X, v_flt Y, v_flt Z) const override final;
+	//~ End FVoxelGeneratorInstance Interface
+
+private:
+	const float NoiseHeight;
+	const int32 Seed;
+	FVoxelFastNoise Noise;
+};
